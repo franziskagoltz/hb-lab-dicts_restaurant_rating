763ab29cbff294144c39f15fdc067972ff314434
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 17:33:35 -0700

    updated and cleaned up restaurant_rater function to be more precise

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -2,32 +2,24 @@
 
 import sys 
 
-restaurant_scores_source = open(sys.argv[1])
+def restaurant_rater():
+    """
+    Prints an alphabetized list of restaurants and their ratings using a dictionary.
+    """
 
-restaurant_ratings = {}
+    restaurant_scores_source = open(sys.argv[1])
+    restaurant_ratings = {}
 
-for line in restaurant_scores_source:
-    #Splits each line at : and creates a list containing two strings for each line
-    restaurant_details = line.split(":")
+    for line in restaurant_scores_source:
+        #Strips and splits each line at : and unpacks list into name and rating
+        name, rating = line.rstrip().split(":")
+
+        #Add keys and values to restaurant_ratings based on name and rating
+        restaurant_ratings[name] = int(rating)
+
+    for restaurant in sorted(restaurant_ratings):
+        print restaurant, "is rated at", restaurant_ratings[restaurant]
     
-    #Strips white space from rating
-    restaurant_details[1] = restaurant_details[1].rstrip() 
+    restaurant_scores_source.close()
 
-    # we set the key of restaurant_ratings to the 0th index of our list (the restaurant)
-    # to the index[1] of our list(the rating of said restaurant)
-    restaurant_ratings[restaurant_details[0]] = int(restaurant_details[1])
-
-sorted_keys = sorted(restaurant_ratings.keys())
-
-for restaurant in sorted_keys:
-    print restaurant, "is rated at", restaurant_ratings[restaurant]
-    
-
-    # sorted_ratings = [key, value]
-
-
-
-
-
-restaurant_scores_source.close()
-
+restaurant_rater()
